32a9bfb78790994e0071a600edd68e923bf1c763
ReimarPB <reimarpb@gmail.com>
2023-06-17 15:13:43 +0200

    Add design for posts

--- public/home/index.php
+++ public/home/index.php
@@ -10,16 +10,20 @@
 		<title>Posts</title>
 	</head>
 	<body>
-		<?php foreach ($result->posts as $post): ?>
-			<div class="post">
-				<a class="post-title" href="<?= $post->post->url ?: "#"?>"><?= $post->post->body ?></a>
-				<br>
-				<span class="post-info">
-					by <a href="/user?id=<?= $post->creator->id ?>"><?= $post->creator->name ?></a>
-					in <?= $post->community->name ?>
-				</span>
-			</div>
-		<?php endforeach; ?>
+		<div id="posts">
+			<?php foreach ($result->posts as $post): ?>
+				<div class="post">
+					<a class="post-title" href="<?= $post->post->url ?: "#"?>" target="_blank">
+						<?= $post->post->body ?>
+					</a>
+					<br>
+					<span class="post-info">
+						by <a href="/user?id=<?= $post->creator->id ?>"><?= $post->creator->name ?></a>
+						in <a href="/community?id=<?= $post->community->id ?>"><?= $post->community->name ?></a>
+					</span>
+				</div>
+			<?php endforeach; ?>
+		</div>
 	</body>
 </html>
 
